d02aee6480d3ae301581fb446638c1419c0482a6
Luis Daniel <creatiposweb@gmail.com>
2020-09-25 16:21:49 -0300

    ss

--- src/audits/field-fcp.js
+++ src/audits/field-fcp.js
@@ -1,7 +1,5 @@
 const { Audit } = require('lighthouse')
 const {
-    getLoadingExperience,
-    createNotApplicableResult,
     createValueResult,
 } = require('../utils/audit-helpers')
 
@@ -9,15 +7,14 @@ module.exports = class FieldFcpAudit extends Audit {
     static get meta() {
         return {
             id: 'field-fcp',
-            title: 'First Contentful Paint (URL)',
+            title: 'Enlaces',
             description: 'First Contentful Paint (FCP) marks the first time in the page load timeline where the user can see anything on the screen. A fast FCP (75th percentile) helps reassure the user that something is happening. [Learn more about FCP](https://web.dev/fcp/)',
             scoreDisplayMode: 'numeric',
             requiredArtifacts: ['URL', 'settings'],
         }
     }
 
-    /** @param {Object} artifacts @param {Object} context */
-    static async audit(artifacts, context) {
+    static async audit() {
         try {
 
 
