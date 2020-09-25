ecce021281173d81eadc44bac8b85469f09220b3
Luis Daniel <creatiposweb@gmail.com>
2020-09-25 16:42:34 -0300

    ss

--- src/audits/field-seo.js
+++ src/audits/field-seo.js
@@ -6,7 +6,7 @@ const {
 module.exports = class FieldFcpAudit extends Audit {
     static get meta() {
         return {
-            id: 'has-cat-images-id',
+            id: 'field-seo',
             title: 'Page has least one cat image',
             failureTitle: 'Page does not have at least one cat image',
             description: 'Pages should have lots of cat images to keep users happy. ' +
