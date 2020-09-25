8dcdc84a15b3649a40ed236d2fbd640906002921
Luis Daniel <creatiposweb@gmail.com>
2020-09-25 15:39:32 -0300

    dd

--- src/audits/field-fcp.js
+++ src/audits/field-fcp.js
@@ -1,32 +1,32 @@
 const { Audit } = require('lighthouse')
 const {
-  getLoadingExperience,
-  createNotApplicableResult,
-  createValueResult,
-  createErrorResult,
-  isResultsInField,
+    getLoadingExperience,
+    createNotApplicableResult,
+    createValueResult,
+    createErrorResult,
+    isResultsInField,
 } = require('../utils/audit-helpers')
 
 module.exports = class FieldFcpAudit extends Audit {
-  static get meta() {
-    return {
-      id: 'field-fcp',
-      title: 'First Contentful Paint (URL)',
-      description:
-        'First Contentful Paint (FCP) marks the first time in the page load timeline where the user can see anything on the screen. A fast FCP (75th percentile) helps reassure the user that something is happening. [Learn more about FCP](https://web.dev/fcp/)',
-      scoreDisplayMode: 'numeric',
-      requiredArtifacts: ['URL', 'settings'],
+    static get meta() {
+        return {
+            id: 'field-fcp',
+            title: 'First Contentful Paint (URL)',
+            description: 'First Contentful Paint (FCP) marks the first time in the page load timeline where the user can see anything on the screen. A fast FCP (75th percentile) helps reassure the user that something is happening. [Learn more about FCP](https://web.dev/fcp/)',
+            scoreDisplayMode: 'numeric',
+            requiredArtifacts: ['URL', 'settings'],
+        }
     }
-  }
 
-  /** @param {Object} artifacts @param {Object} context */
-  static async audit(artifacts, context) {
-    try {
-      const le = await getLoadingExperience(artifacts, context)
-      if (!isResultsInField(le)) return createNotApplicableResult(FieldFcpAudit.meta.title)
-      return createValueResult(le.metrics.FIRST_CONTENTFUL_PAINT_MS, 'fcp')
-    } catch (err) {
-      return createErrorResult(err)
+    /** @param {Object} artifacts @param {Object} context */
+    static async audit(artifacts, context) {
+        try {
+            const le = await getLoadingExperience(artifacts, context)
+            if (!isResultsInField(le)) return createNotApplicableResult(FieldFcpAudit.meta.title)
+            console.log(createValueResult(le.metrics.FIRST_CONTENTFUL_PAINT_MS, 'fcp'))
+            return createValueResult(le.metrics.FIRST_CONTENTFUL_PAINT_MS, 'fcp')
+        } catch (err) {
+            return createErrorResult(err)
+        }
     }
-  }
 }
--- src/index.js
+++ src/index.js
@@ -15,60 +15,59 @@ const scoreAllCategories = ReportScoring.scoreAllCategories
 /** @typedef {{ score: ?number, auditRefs: { id: string, weight: number }[] }} CategoryResult */
 
 /** @param {any} configCategories @param {ResultsByAuditId} resultsByAuditId */
-ReportScoring.scoreAllCategories = function (configCategories, resultsByAuditId) {
-  const result = scoreAllCategories(configCategories, resultsByAuditId)
-  const fieldPluginCategory = /** @type {CategoryResult | null} */ (result['lighthouse-plugin-field-performance'])
-  if (!fieldPluginCategory) return result
-  fieldPluginCategory.score = getMinScore(fieldPluginCategory, resultsByAuditId)
-  return result
+ReportScoring.scoreAllCategories = function(configCategories, resultsByAuditId) {
+    const result = scoreAllCategories(configCategories, resultsByAuditId)
+    const fieldPluginCategory = /** @type {CategoryResult | null} */ (result['lighthouse-plugin-field-performance'])
+    if (!fieldPluginCategory) return result
+    fieldPluginCategory.score = getMinScore(fieldPluginCategory, resultsByAuditId)
+    return result
 }
 
 /** @param {CategoryResult} fieldPluginCategoryResult @param {ResultsByAuditId} resultsByAuditId */
 function getMinScore(fieldPluginCategoryResult, resultsByAuditId) {
-  const activeAuditRefs = fieldPluginCategoryResult.auditRefs.filter((auditRef) => auditRef.weight !== 0)
-  const scores = activeAuditRefs.map((auditRef) => resultsByAuditId[auditRef.id].score)
-  return scores.length ? Math.min(...scores) : 0
+    const activeAuditRefs = fieldPluginCategoryResult.auditRefs.filter((auditRef) => auditRef.weight !== 0)
+    const scores = activeAuditRefs.map((auditRef) => resultsByAuditId[auditRef.id].score)
+    return scores.length ? Math.min(...scores) : 0
 }
 
 module.exports = {
-  audits: [
-    { path: 'lighthouse-plugin-field-performance/src/audits/field-fcp.js' },
-    { path: 'lighthouse-plugin-field-performance/src/audits/field-lcp.js' },
-    { path: 'lighthouse-plugin-field-performance/src/audits/field-fid.js' },
-    { path: 'lighthouse-plugin-field-performance/src/audits/field-cls.js' },
-    { path: 'lighthouse-plugin-field-performance/src/audits/field-fcp-origin.js' },
-    { path: 'lighthouse-plugin-field-performance/src/audits/field-lcp-origin.js' },
-    { path: 'lighthouse-plugin-field-performance/src/audits/field-fid-origin.js' },
-    { path: 'lighthouse-plugin-field-performance/src/audits/field-cls-origin.js' },
-  ],
-  groups: {
-    page: {
-      title: 'Page summary',
-    },
-    origin: {
-      title: 'Origin summary',
-    },
-  },
-  category: {
-    title: 'Field Performance',
-    description:
-      'These metrics show the performance of the page over the past 30 days. Data is collected anonymously in for real-world Chrome users and provided by Chrome UX Report. [Learn More](https://developers.google.com/web/tools/chrome-user-experience-report/)',
-    auditRefs: [
-      // Now every metric is weighted equally, based on idea to pass all CWV.
-      //
-      // alternative weights (based on Lighthouse):
-      // 15 (FCP)
-      // 25 + 15 = 40 (SI + LCP)
-      // 15 + 25 = 40 (TTI + TBT)
-      // 5 (CLS)
-      { id: 'field-fcp', weight: 0, group: 'page' },
-      { id: 'field-lcp', weight: 1, group: 'page' },
-      { id: 'field-fid', weight: 1, group: 'page' },
-      { id: 'field-cls', weight: 1, group: 'page' },
-      { id: 'field-fcp-origin', weight: 0, group: 'origin' },
-      { id: 'field-lcp-origin', weight: 0, group: 'origin' },
-      { id: 'field-fid-origin', weight: 0, group: 'origin' },
-      { id: 'field-cls-origin', weight: 0, group: 'origin' },
+    audits: [
+        { path: 'lighthouse-plugin-field-performance/src/audits/field-fcp.js' },
+        { path: 'lighthouse-plugin-field-performance/src/audits/field-lcp.js' },
+        { path: 'lighthouse-plugin-field-performance/src/audits/field-fid.js' },
+        { path: 'lighthouse-plugin-field-performance/src/audits/field-cls.js' },
+        { path: 'lighthouse-plugin-field-performance/src/audits/field-fcp-origin.js' },
+        { path: 'lighthouse-plugin-field-performance/src/audits/field-lcp-origin.js' },
+        { path: 'lighthouse-plugin-field-performance/src/audits/field-fid-origin.js' },
+        { path: 'lighthouse-plugin-field-performance/src/audits/field-cls-origin.js' },
     ],
-  },
+    groups: {
+        page: {
+            title: 'Page summary',
+        },
+        origin: {
+            title: 'Origin summary',
+        },
+    },
+    category: {
+        title: 'Backlink Checker de Ahrefs',
+        description: 'Try the free version of Ahrefs Backlink Checker. Get a glimpse into the power of our premium tool.',
+        auditRefs: [
+            // Now every metric is weighted equally, based on idea to pass all CWV.
+            //
+            // alternative weights (based on Lighthouse):
+            // 15 (FCP)
+            // 25 + 15 = 40 (SI + LCP)
+            // 15 + 25 = 40 (TTI + TBT)
+            // 5 (CLS)
+            { id: 'field-fcp', weight: 0, group: 'page' },
+            { id: 'field-lcp', weight: 1, group: 'page' },
+            { id: 'field-fid', weight: 1, group: 'page' },
+            { id: 'field-cls', weight: 1, group: 'page' },
+            { id: 'field-fcp-origin', weight: 0, group: 'origin' },
+            { id: 'field-lcp-origin', weight: 0, group: 'origin' },
+            { id: 'field-fid-origin', weight: 0, group: 'origin' },
+            { id: 'field-cls-origin', weight: 0, group: 'origin' },
+        ],
+    },
 }
