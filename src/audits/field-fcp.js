f27b831f21452253335f10645698bde14493ddbe
Luis Daniel <creatiposweb@gmail.com>
2020-09-25 16:32:16 -0300

    demo

--- src/audits/field-fcp.js
+++ src/audits/field-fcp.js
@@ -10,17 +10,11 @@ module.exports = class FieldFcpAudit extends Audit {
             title: 'Enlaces',
             description: 'First Contentful Paint (FCP) marks the first time in the page load timeline where the user can see anything on the screen. A fast FCP (75th percentile) helps reassure the user that something is happening. [Learn more about FCP](https://web.dev/fcp/)',
             scoreDisplayMode: 'numeric',
-            requiredArtifacts: ['URL', 'settings'],
+            requiredArtifacts: ['URL', 'ImageElements'],
         }
     }
 
     static async audit() {
-        try {
-
-
-            return createValueResult()
-        } catch (err) {
-            return null
-        }
+        return createValueResult()
     }
 }
--- src/utils/audit-helpers.js
+++ src/utils/audit-helpers.js
@@ -1,7 +1,3 @@
-const { round } = require('lodash')
-const { Audit } = require('lighthouse')
-const { runPsi } = require('./run-psi')
-
 /**
  * @typedef {'fcp' | 'lcp' | 'fid' | 'cls'} Metric
  * @typedef {{ percentile: number, distributions: { min: number, max: number, proportion: number}[] }} MetricValue
@@ -11,36 +7,8 @@ const { runPsi } = require('./run-psi')
 
 // cache PSI requests
 
-const requests = new Map()
 
-/**
- * Cache results and parse crux data.
- *
- * @param {any} artifacts
- * @param {any} context
- * @param {boolean} [isUrl]
- * @return {Promise<LoadingExperience>}
- */
 
-exports.getLoadingExperience = async(artifacts, context, isUrl = true) => {
-    const psiToken = context.settings.psiToken || null
-    const strategy = artifacts.settings.emulatedFormFactor === 'desktop' ? 'desktop' : 'mobile'
-    const prefix = isUrl ? 'url' : 'origin'
-    const { href, origin } = new URL(artifacts.URL.finalUrl)
-    const url = `${prefix}:${href}`
-    const key = url + strategy
-    if (!requests.has(key)) {
-        requests.set(key, runPsi({ url, strategy, psiToken }))
-    }
-    const json = await requests.get(key)
-    if (json.error) throw new Error(JSON.stringify(json.error))
-        // check, that URL response is not for origin
-    if (isUrl) {
-        const hasUrlExperience = json.loadingExperience && json.loadingExperience.id !== origin
-        return hasUrlExperience ? json.loadingExperience : null
-    }
-    return json.loadingExperience
-}
 
 /**
  * Estimate value and create numeric results
@@ -55,20 +23,5 @@ exports.createValueResult = () => {
         score: 39,
         //numericUnit: 39,
         displayValue: 39
-    }
-}
-
-/**
- * Create result when data does not exist.
- *
- * @param {string} title
- */
-
-exports.createNotApplicableResult = (title) => {
-    return {
-        score: 39,
-        notApplicable: true,
-        explanation: `The Chrome User Experience Report 
-          does not have sufficient real-world ${title} data for this page.`,
     }
 }
