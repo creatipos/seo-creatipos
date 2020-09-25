c41bec198ff661262579774306352d5f61f48a11
Luis Daniel <creatiposweb@gmail.com>
2020-09-25 16:41:48 -0300

    dd

--- src/audits/field-seo.js
+++ src/audits/field-seo.js
@@ -6,11 +6,12 @@ const {
 module.exports = class FieldFcpAudit extends Audit {
     static get meta() {
         return {
-            id: 'field-seo',
-            title: 'Enlaces',
-            description: 'seo',
-            scoreDisplayMode: 'numeric',
-            requiredArtifacts: ['URL', 'ImageElements'],
+            id: 'has-cat-images-id',
+            title: 'Page has least one cat image',
+            failureTitle: 'Page does not have at least one cat image',
+            description: 'Pages should have lots of cat images to keep users happy. ' +
+                'Consider adding a picture of a cat to your page improve engagement.',
+            requiredArtifacts: ['ImageElements'],
         }
     }
 
--- src/utils/audit-helpers.js
+++ src/utils/audit-helpers.js
@@ -1,16 +1,4 @@
 /**
- * @typedef {'fcp' | 'lcp' | 'fid' | 'cls'} Metric
- * @typedef {{ percentile: number, distributions: { min: number, max: number, proportion: number}[] }} MetricValue
- * @typedef {{ id: string, overall_category: string, initial_url: string
-               metrics: { FIRST_INPUT_DELAY_MS: MetricValue, FIRST_CONTENTFUL_PAINT_MS: MetricValue, LARGEST_CONTENTFUL_PAINT_MS: MetricValue, CUMULATIVE_LAYOUT_SHIFT_SCORE: MetricValue } }} LoadingExperience
- */
-
-// cache PSI requests
-
-
-
-
-/**
  * Estimate value and create numeric results
  *
  * @return {Object}
